76527a7993f94def2864ba22943682718cc93efb
RapboyGao <atgao74201@qq.com>
2025-04-20 10:36:26 +0800

    2025.04.20 更新PepecChoosing组件，添加打乱顺序的复选框并修复问题显示逻辑

--- src/.vuepress/components/pepec-choosing/PepecChoosing.vue
+++ src/.vuepress/components/pepec-choosing/PepecChoosing.vue
@@ -5,18 +5,27 @@
     </ElLink>
   </div>
   <ElDivider></ElDivider>
-  <div v-for="(item, index) in choosingQuestions" :key="item._id" class="mt-50" :id="`pepec-${index}`">
+  <div class="flex center">
+    <el-checkbox v-model="useShuffle" label="打乱顺序" size="large" />
+  </div>
+  <ElDivider></ElDivider>
+  <div v-for="(item, index) in shownQuestions" :key="item._id" class="mt-50" :id="`pepec-${index}`">
     <PepecChoosingCard :index="index + 1" :question="item"></PepecChoosingCard>
   </div>
 </template>
 <script setup lang="ts">
-import { ElDivider, ElLink } from "element-plus";
-import { defineProps } from "vue";
+import { ElDivider, ElLink, ElCheckbox } from "element-plus";
+import { computed, defineProps, ref } from "vue";
 import type { PepecChoosing } from "../pepec.types";
 import PepecChoosingCard from "./PepecChoosingCard.vue";
+import _ from "lodash";
 const props = defineProps<{
   choosingQuestions: PepecChoosing[];
 }>();
 let totalLength = props.choosingQuestions;
 let pages = Math.ceil(totalLength.length / 50 - 1);
+let useShuffle = ref(false);
+let shownQuestions = computed(() => {
+  return useShuffle.value ? _.shuffle(totalLength) : totalLength;
+});
 </script>
