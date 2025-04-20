2b07fa6d83deab4abefca8f586eb4753fb394338
RapboyGao <atgao74201@qq.com>
2025-04-20 10:50:38 +0800

    2025.04.20 优化PepecChoosing组件，调整模板结构以提高可读性，移除未使用的复选框和相关逻辑

--- src/.vuepress/components/pepec-choosing/PepecChoosing.vue
+++ src/.vuepress/components/pepec-choosing/PepecChoosing.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
+  <div class="flex-wrap center">
     <ElLink v-for="pageIndex in pages" :key="pageIndex" :href="`#choosing-${pageIndex * 50}`" class="margin-left-right" type="primary">
       {{ pageIndex * 50 }}
     </ElLink>
   </div>
   <ElDivider></ElDivider>
-  <div class="flex center">
+  <div class="flex-wrap center">
     <el-checkbox v-model="useShuffle" label="Shuffled" size="large" class="margin-left-right" />
-    <el-checkbox v-model="showFavoriteOnly" label="Favorites Only" size="large" class="margin-left-right" />
   </div>
-  <ElDivider></ElDivider>
   <div v-for="(item, index) in shownQuestions" :key="item._id" class="mt-50" :id="`pepec-${index}`">
     <PepecChoosingCard :index="index + 1" :question="item"></PepecChoosingCard>
   </div>
@@ -26,16 +24,25 @@ const props = defineProps<{
 }>();
 
 let useShuffle = ref(false);
-let showFavoriteOnly = ref(false);
-let favoriteQuestions = computed(() => {
-  return showFavoriteOnly.value
-    ? props.choosingQuestions.filter((item) => {
-        return store.get("favorite-" + item._id, false);
-      })
-    : props.choosingQuestions;
-});
+// let showFavoriteOnly = ref(false);
+
+// let favoriteQuestions = computed(() => {
+//   if (showFavoriteOnly.value) {
+//     const result = props.choosingQuestions.filter((item) => {
+//       return store.get("favorite-" + item._id, false);
+//     });
+//     console.log("favoriteQuestions", result);
+//     return result;
+//   } else {
+//     return props.choosingQuestions;
+//   }
+// });
+
+// let shownQuestions = computed(() => {
+//   return useShuffle.value ? _.shuffle(favoriteQuestions.value) : favoriteQuestions.value;
+// });
 let shownQuestions = computed(() => {
-  return useShuffle.value ? _.shuffle(favoriteQuestions.value) : favoriteQuestions.value;
+  return useShuffle.value ? _.shuffle(props.choosingQuestions) : props.choosingQuestions;
 });
 
 let pages = computed(() => Math.ceil(shownQuestions.value.length / 50 - 1));
